43c44983e4eda145eefcd0f3e0f1d6f3ddfcdf01
henry <henry@iMac-home.local>
2022-02-19 16:49:08 +0900

    sola system, <mesh> is a object3D ...

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Stars } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
 import styled from "styled-components";
@@ -29,8 +29,26 @@ const Aniamtion = (props) => {
   return null;
 };
 
+const EarthAniamtion = (props) => {
+  useFrame(({ clock }) => {
+    props.EarthBox.current.rotation.y =
+      clock.getElapsedTime() * ((100 * Math.PI) / 180);
+  });
+  return null;
+};
+
+const MoonAniamtion = (props) => {
+  useFrame(({ clock }) => {
+    props.MoonBox.current.rotation.y =
+      clock.getElapsedTime() * ((200 * Math.PI) / 180);
+  });
+  return null;
+};
+
 const SolarSystem = () => {
   const thisBox = useRef();
+  const EarthBox = useRef();
+  const MoonBox = useRef();
 
   const Sun = () => {
     return (
@@ -39,6 +57,7 @@ const SolarSystem = () => {
           <mesh>
             <sphereGeometry attach="geometry" args={[1, 16, 16]} />
             <meshPhongMaterial attach="material" color="orangered" />
+            <Earth />
           </mesh>
           <mesh>
             <sphereGeometry attach="geometry" args={[1, 16, 16]} />
@@ -53,16 +72,36 @@ const SolarSystem = () => {
   const Earth = () => {
     return (
       <>
-        <group position={[3, 0, 0]}>
+        <group ref={EarthBox} position={[3, 0, 0]}>
           <mesh>
             <sphereGeometry attach="geometry" args={[0.3, 16, 16]} />
             <meshPhongMaterial attach="material" color="darkgreen" />
+            <Moon />
           </mesh>
           <mesh>
             <sphereGeometry attach="geometry" args={[0.3, 16, 16]} />
             <meshNormalMaterial wireframe />
           </mesh>
         </group>
+        <EarthAniamtion EarthBox={EarthBox} />
+      </>
+    );
+  };
+
+  const Moon = () => {
+    return (
+      <>
+        <group ref={MoonBox} position={[0.5, 0, 0]}>
+          <mesh>
+            <sphereGeometry attach="geometry" args={[0.05, 16, 16]} />
+            <meshPhongMaterial attach="material" color="honeydew" />
+          </mesh>
+          <mesh>
+            <sphereGeometry attach="geometry" args={[0.1, 16, 16]} />
+            <meshNormalMaterial wireframe />
+          </mesh>
+        </group>
+        <MoonAniamtion MoonBox={MoonBox} />
       </>
     );
   };
@@ -71,7 +110,7 @@ const SolarSystem = () => {
     <>
       <CanvasContainer>
         <Title>Scene Graph : Solar System</Title>
-        <Canvas>
+        <Canvas camera={{ position: [-1, 5, 5] }}>
           <Suspense fallback={null}>
             <ambientLight />
             <directionalLight
@@ -80,8 +119,7 @@ const SolarSystem = () => {
               intensity={3}
             />
             <Sun />
-            <Earth />
-
+            <Stars count={1000} factor={3} />
             <OrbitControls />
           </Suspense>
         </Canvas>
