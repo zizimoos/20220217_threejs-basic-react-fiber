00f0d45229abf0d89e6fced5c1e357a6f2ffd148
henry <henry@iMac-home.local>
2022-03-01 07:33:39 +0900

    eagle view done

--- src/components/IntroSystem.jsx
+++ src/components/IntroSystem.jsx
@@ -35,43 +35,106 @@ const Plane = () => {
   );
 };
 
+// const Box = () => {
+//   const [isClicked, setIsClicked] = useState(false);
+//   const { scale } = useSpring({
+//     scale: isClicked ? 1.5 : 1,
+//     config: { mass: 5, tension: 500, friction: 80 },
+//   });
+//   return (
+//     <animated.mesh
+//       scale={scale}
+//       onClick={() => setIsClicked(!isClicked)}
+//       position={[0, 1, 0]}
+//     >
+//       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+//       <meshStandardMaterial attach="material" color="blue" visible={true} />
+//     </animated.mesh>
+//   );
+// };
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+const mesh = {
+  position: null,
+};
+
 const Box = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  const { scale } = useSpring({
-    scale: isClicked ? 1.5 : 1,
-    config: { mass: 5, tension: 500, friction: 80 },
-  });
+  const box = useRef();
+  mesh.position = box;
   return (
-    <animated.mesh
-      scale={scale}
-      onClick={() => setIsClicked(!isClicked)}
-      position={[0, 1, 0]}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="blue" visible={true} />
-    </animated.mesh>
+    <mesh ref={box}>
+      <meshStandardMaterial color="hotpink" />
+      <boxGeometry args={[1, 1, 1, 5, 5, 5]} />
+    </mesh>
   );
 };
 
-function IntroSystem() {
-  const BoxRef = useRef();
+const Camera = () => {
+  const camera = useRef();
   const cameraRef = useRef();
+  const BoxRef = useRef();
 
-  const [ref, camera] = useResource();
+  const RefonClick = () => {
+    console.log("clicked");
+    const { x, y, z } = cameraRef.current.position;
+    cameraRef.current.position.set(x, y, z + 1);
+
+    camera.current.position.z = z;
+    camera.current.lookAt(cameraRef.current.position);
+  };
+
+  document.onkeydown = (e) => {
+    switch (e.keyCode) {
+      case 37:
+        cameraRef.current.position.x = cameraRef.current.position.x -= 0.5;
+        break;
+      case 39:
+        cameraRef.current.position.x = cameraRef.current.position.x += 0.5;
+        break;
+      case 38:
+        cameraRef.current.position.z = cameraRef.current.position.z -= 0.5;
+        break;
+      case 40:
+        cameraRef.current.position.z = cameraRef.current.position.z += 0.5;
+        break;
+      default:
+    }
+    const { x, y, z } = cameraRef.current.position;
+    cameraRef.current.position.set(x, y, z);
+    camera.current.position.x = x;
+    camera.current.position.z = z;
+    camera.current.position.y = y;
+
+    if (camera.current && cameraRef.position.current) {
+      camera.current.position.y = y;
+      camera.current.position.z = z;
+      camera.current.position.x = x;
+      camera.current.lookAt(cameraRef.position.current.position);
+    }
+  };
 
   return (
-    <Canvas shadows>
-      <group position={[0, -2, 0]}>
-        <PerspectiveCamera
-          ref={cameraRef}
-          position={[0, 5, 1]}
-          fov={75}
-          aspect={sizes.aspects}
-          near={0.1}
-          far={1000}
-        >
-          <meshBasicMaterial />
-        </PerspectiveCamera>
+    <perspectiveCamera
+      ref={camera}
+      fov={75}
+      aspect={sizes.width / sizes.height}
+      near={0.1}
+      far={100}
+    >
+      <Box />
+      <group position={[0, 0, 0]}>
+        <mesh ref={cameraRef} onClick={RefonClick}>
+          <boxBufferGeometry
+            attach="geometry"
+            args={[3, 3, 3]}
+            position={(1, 0, 0)}
+          />
+          <meshStandardMaterial attach="material" color="white" />
+        </mesh>
 
         <Box />
         <mesh ref={BoxRef} position={[0, 0, -3]}>
@@ -80,9 +143,16 @@ function IntroSystem() {
         </mesh>
         <Plane />
       </group>
+    </perspectiveCamera>
+  );
+};
+
+function IntroSystem() {
+  return (
+    <Canvas shadows>
+      <Camera />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-
       <OrbitControls />
     </Canvas>
   );
